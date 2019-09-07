facd6a112b6b4e7256e061b3d7a0314d2594a4ec
Jonathan Ray <jonathanray17@gmail.com>
2019-09-07 11:39:17 -0400

    removes exposed endpoint URL

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import ExchangeRates from './ExchangeRates';
-
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: BASE_URL,
 });
 
 export default () => (
   <ApolloProvider client={client}>
     <div>
-      <h2>My first Apollo app 🚀</h2>
+      <h2>My first Apollo app 
+        <span role="img" aria-label="jsxal1y/accessible-emoji" >🚀</span>
+      </h2>
       <ExchangeRates />
     </div>
   </ApolloProvider>
